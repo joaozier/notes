313d07ac5cf370245cb011e1ece3b82160207193
joaozier <165683079+joaozier@users.noreply.github.com>
2024-11-29 18:32:13 -0300

    Update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Container\Attributes\DB;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -15,15 +16,32 @@ class AuthController extends Controller
         // validação de formulário
         $request->validate(
             [
-                'text_username'=>'required', //diz ao laravel que deve ter algo dentro desse campo
-                'text_password'=>'required'
+                'text_username'=>'required|email', //diz ao laravel que deve ter algo dentro desse campo
+                'text_password'=>'required|min:6|max:16'
+            ],
+            //mensagens de erro
+            [
+                'text_username.required'=>'O usuário é obrigatório',
+                'text_username.email'=>'Usuário deve ser um email válido',
+                'text_password.required'=>'A senha é obrigatória.',
+                'text_password.min'=>'A senha deve ter no mínimo :min caracteres',
+                'text_password.max'=>'A senha deve ter no mínimo :max caracteres'
             ]
         );
         //get user input
         $username=$request->input('text_username');
         $password=$request->input('text_password');
 
-        echo "OK";
+        //teste da conexão da base de dados
+        try {
+            DB::connection()->getPdo();
+            echo 'Conexão feita.';
+        } catch (\PDOException $e) {
+            echo "Conexão falhou: ".$e->getMessage();
+        }
+
+        echo "<br>Fim.";
+
     }
     public function logout()
     {
