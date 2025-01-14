694df0a39dbaa0492611d7a30ddbf66ea99ed6db
joaozier <165683079+joaozier@users.noreply.github.com>
2025-01-14 18:03:43 -0300

    Update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index(){
-        return view('home');
+        $id=session('user.id');
+        $notes=User::find($id)->notes()->get()->toArray();
+
+        return view('home',['notes'=>$notes]);
     }
 
     // public function index(){
